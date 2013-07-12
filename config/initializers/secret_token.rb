0acf5a1e02840cf63d850cf40f6cc4ccea601429
Hassan Schroeder <hassan@webtuitive.com>
2013-07-12 16:41:44 -0700

    initial test app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Raw::Application.config.secret_token = '607ab9eb98828a4bdcf665374eb92f23f1a55272bd003e33cd5daa450d60265984e4a6fa3276c719ee1cfdcdd0315e473e8c8a87cdf047932de6d8ac03ce7ec7'
